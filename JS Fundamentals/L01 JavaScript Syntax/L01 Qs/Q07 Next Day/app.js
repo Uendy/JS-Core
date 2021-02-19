d0a384135efcdd235d017eabc95099d73db5737a
Uendy <alex.beshkov98@gmail.com>
2021-02-19 16:02:16 +0200

    Update: -Re-did Q07 Next Day (much cleaner, though I miss AddDays() from C#)

--- JS Fundamentals/L01 JavaScript Syntax/L01 Qs/Q07 Next Day/app.js
+++ JS Fundamentals/L01 JavaScript Syntax/L01 Qs/Q07 Next Day/app.js
@@ -2,11 +2,10 @@
 
 function addDay(year, month, day) {
     let date = new Date(year, month - 1, day);
-    let oneDay = 24 * 60 * 60 * 1000; 
-    let nextDate = new Date(date.getTime() + oneDay);
-    return nextDate.getFullYear() + "-" +
-        (nextDate.getMonth() + 1) + "-" +
-        nextDate.getDate();
-}
+    date.setDate(date.getDate() + 1);
+    return date.getFullYear() + "-" +
+        (date.getMonth() + 1) + "-" +
+        date.getDate();
+    }
 
 console.log(addDay(2016, 9, 30));
