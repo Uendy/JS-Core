88f4177cd0da6a5739f90a477f7730c81c8f7808
Uendy <alex.beshkov98@gmail.com>
2021-02-28 12:57:47 +0200

    Update: -Finished: Q19 Quadratic Equation.js

--- JS Fundamentals/L02 Operators and Logic/L02 Qs/Q19 Quadratic Equation.js
+++ JS Fundamentals/L02 Operators and Logic/L02 Qs/Q19 Quadratic Equation.js
@@ -3,11 +3,24 @@ function quadEquation(input) {
     let b = input[1];
     let c = input[2];
 
-    firstX = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-    secondX = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    let d = Math.pow(b, 2) - 4 * a * c;
+    if(d > 0){
+        let firstX = ((-b) + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+        let secondX = ((-b) - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
 
-    console.log(Math.min(firstX, secondX));
-    console.log(Math.max(firstX, secondX));
+        console.log(Math.min(firstX, secondX));
+        console.log(Math.max(firstX, secondX)); 
+    }
+    else if(d === 0){
+        let x = (-b) / (2 * a);
+        console.log(x);
+    }
+    else{ 
+        // d < 0
+        console.log("No");
+    }
 }
 
 quadEquation([6, 11, -35]);
+quadEquation([1, -12, 36]);
+quadEquation([5, 2, 1]);
