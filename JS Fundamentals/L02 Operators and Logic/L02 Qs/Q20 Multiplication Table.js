5ebb3d9d8e08e3e96ffdca6f10438a5c27d698f8
Uendy <alex.beshkov98@gmail.com>
2021-03-01 11:01:06 +0200

    Update: -Still cant figure it out

--- JS Fundamentals/L02 Operators and Logic/L02 Qs/Q20 Multiplication Table.js
+++ JS Fundamentals/L02 Operators and Logic/L02 Qs/Q20 Multiplication Table.js
@@ -1,14 +1,21 @@
 function createMultiplicationTable(end) {
     console.log(`<table border="1">`);
 
-    let prefix = "<th>";
-    let postfix = "</th>";
+    let prefix = "<td>";
+    let postfix = "</td>";
+
+    let boldPrefix = "<th>";
+    let boldPostfix = "</th>"
 
     for (let row = 1; row <= end; row++) {
         let line = " <tr>";
-        for (let col = 0; col <= end; col++) {
-            if(row === 1 && col === 0){
+        for (let col = 1; col <= end; col++) {
+            if(row === 1 && col === 1){
                 line += prefix + "x" + postfix;
+            }
+            else if(row === 1 || col === 1)
+            {
+                line += "<th>" + `${row * col}` + "</th>"                
             }
             else{
                 line += prefix + (row * col) + postfix;
