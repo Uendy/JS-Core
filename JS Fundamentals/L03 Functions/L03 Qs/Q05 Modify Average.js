0bd8beaf3a49fad92f356a69026ca096a43329f4
Uendy <alex.beshkov98@gmail.com>
2021-03-04 19:01:55 +0200

    Adding: Q05 Modify Average.js
    // Did it in a very C# way trying to use type conversions

---
+++ JS Fundamentals/L03 Functions/L03 Qs/Q05 Modify Average.js
@@ -0,0 +1,22 @@
+function modifyAverage(number) {
+    
+    function getAverage(number) {
+        let sum = 0;
+        let numAsString = number.toString().split('');
+        numAsString.forEach(num => {
+            sum += parseInt(num);
+        });
+        let average = sum / numAsString.length;
+        return average;
+    }
+
+    while (getAverage(number) <= 5) {
+        number += "9";
+        getAverage(number);
+    }
+
+    console.log(number);
+}
+
+modifyAverage(5835);
+modifyAverage(101);
