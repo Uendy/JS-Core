266b60cc90723f5956935036cc0ae8704668adef
Uendy <alex.beshkov98@gmail.com>
2021-03-04 18:01:32 +0200

    Update: -Redid Q04 Cooking by Numbers.js with Lambda functions

--- JS Fundamentals/L03 Functions/L03 Qs/Q04 Cooking by Numbers.js
+++ JS Fundamentals/L03 Functions/L03 Qs/Q04 Cooking by Numbers.js
@@ -1,25 +1,11 @@
 function cook(input) {
     let number = input[0];
     
-    function chop(number) { 
-        return number / 2;
-    }
-
-    function dice(number) {
-        return Math.sqrt(number);
-    }
-
-    function spice (number){
-        return number + 1;
-    }
-
-    function bake(number) {
-        return number * 3;
-    }
-
-    function fillet(number) {
-        return number * 0.80;
-    }
+    let chop = (number) => number / 2;
+    let dice = (number) => Math.sqrt(number);
+    let spice = (number) => number + 1;
+    let bake = (number) => number * 3;
+    let fillet = (number) => number * 0.80;
 
     for (let i = 1; i < input.length; i++){
         let command = input[i];
