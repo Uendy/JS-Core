013d2069ce07d75ef2fff0fce11477204f134849
Uendy <alex.beshkov98@gmail.com>
2021-03-06 20:23:21 +0200

    Update: - I got really lost in VS Code, the debugger is skipping the break points when I dont have the miramac extension :( - When I have the extension it wont let me run at all. I am not sure how to proceed.

--- JS Fundamentals/L03 Functions/L03 Qs/Q09 Radio Crystals.js
+++ JS Fundamentals/L03 Functions/L03 Qs/Q09 Radio Crystals.js
@@ -1,81 +1,84 @@
 function tuneCrystal(input) {
     let desiredFreq = input[0];
-    let currentFreq = input[1];
 
-    console.log(`Processing chunk ${currentFreq} microns`);
+    for (let i = 1; i < input.length; i++) {
+        let currentFreq = input[i];
 
-    function cut(currentFreq) {
-        return currentFreq / 4;
-    }
+        console.log(`Processing chunk ${currentFreq} microns`);
 
-    function lap(currentFreq) {
-        return currentFreq * 0.8;
-    }
+        function cut(currentFreq) {
+            return currentFreq / 4;
+        }
 
-    function grind(currentFreq) {
-        return currentFreq -= 20;
-    }
+        function lap(currentFreq) {
+            return currentFreq * 0.8;
+        }
 
-    function etch(currentFreq) {
-        return currentFreq -= 2;
-    }
+        function grind(currentFreq) {
+            return currentFreq -= 20;
+        }
 
-    function xRay(currentFreq) {
-        return currentFreq += 1;
-    }
+        function etch(currentFreq) {
+            return currentFreq -= 2;
+        }
 
-    function transport(currentFreq) {
-        console.log("Transporting and washing");
-        return Math.floor(currentFreq);
-    }
+        function xRay(currentFreq) {
+            return currentFreq += 1;
+        }
 
-    let cuts = 0;
-    while (desiredFreq <= currentFreq / 4) {
-        currentFreq = cut(currentFreq);
-        cuts++;
-    }
-    if(cuts !== 0){
-        console.log("Cut x" + cuts);
-        transport(currentFreq);
-    }
+        function transport(currentFreq) {
+            console.log("Transporting and washing");
+            return Math.floor(currentFreq);
+        }
 
-    let laps = 0;
-    while (desiredFreq <= currentFreq * 0.8) {
-        currentFreq = lap(currentFreq);
-        laps++;
-    }
-    if(laps !== 0){
-        console.log("Lap x" + laps);
-        transport(currentFreq);
-    }
+        let cuts = 0;
+        while (desiredFreq <= currentFreq / 4) {
+            currentFreq = cut(currentFreq);
+            cuts++;
+        }
+        if (cuts !== 0) {
+            console.log("Cut x" + cuts);
+            transport(currentFreq);
+        }
 
-    let grinds = 0;
-    while (desiredFreq <= currentFreq - 20) {
-        currentFreq = grind(currentFreq);
-        grinds++;
-    }
-    if(grinds !== 0){
-        console.log("Grind x" + grinds);
-        transport(currentFreq);
-    }
+        let laps = 0;
+        while (desiredFreq <= currentFreq * 0.8) {
+            currentFreq = lap(currentFreq);
+            laps++;
+        }
+        if (laps !== 0) {
+            console.log("Lap x" + laps);
+            transport(currentFreq);
+        }
 
-    let etches = 0;
-    while (desiredFreq <= currentFreq) {
-        currentFreq = etch(currentFreq);
-        etches++;
-    }
-    if(etches !== 0){
-        console.log("Etch x" + etches);
-        transport(currentFreq);
-    }
+        let grinds = 0;
+        while (desiredFreq <= currentFreq - 20) {
+            currentFreq = grind(currentFreq);
+            grinds++;
+        }
+        if (grinds !== 0) {
+            console.log("Grind x" + grinds);
+            transport(currentFreq);
+        }
 
-    if(desiredFreq === currentFreq + 1){
-        currentFreq = xRay(currentFreq);
-        console.log("X-ray x1")
-        //transport(currentFreq);
-    }
+        let etches = 0;
+        while (desiredFreq <= currentFreq) {
+            currentFreq = etch(currentFreq);
+            etches++;
+        }
+        if (etches !== 0) {
+            console.log("Etch x" + etches);
+            transport(currentFreq);
+        }
 
-    console.log(`Finished crystal ${currentFreq} microns`);
+        if (desiredFreq === currentFreq + 1) {
+            currentFreq = xRay(currentFreq);
+            console.log("X-ray x1")
+        }
+
+        console.log(`Finished crystal ${currentFreq} microns`);
+    }
 }
 
-tuneCrystal([1375, 50000]);
+tuneCrystal(1000, 4000, 8100);
+//tuneCrystal([1375, 50000]);
