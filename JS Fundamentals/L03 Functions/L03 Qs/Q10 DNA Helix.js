faf2746fabe6b7b3d2539b28b5ba354a5b30564a
Uendy <alex.beshkov98@gmail.com>
2021-03-08 18:22:14 +0200

    Update: -Progress on Q10 DNA Helix.js (Cant use a queue though)

--- JS Fundamentals/L03 Functions/L03 Qs/Q10 DNA Helix.js
+++ JS Fundamentals/L03 Functions/L03 Qs/Q10 DNA Helix.js
@@ -12,16 +12,26 @@ function printDNA(lines) {
     //*G--G*
 
     let queue = ["A", "T", "C", "G", "T", "T", "A", "G", "G", "G"];
+    
 
     function rotateQueue(queue) {
-        let temporary = queue.Peek();
-        queue.Pop()
-        queue.Push(temporary);
+        let temporary = queue[0];
+        queue.shift();
+        queue.push(temporary);
+        queue = queue;
+        return temporary;
     }
 
     let stars = 2;
     let dashes = 0;
-    for (let line = 0; line < lines; index++) {
+    for (let line = 0; line < lines; line++) {
 
+        let output = "*".repeat(stars) + "-".repeat(dashes) +
+            rotateQueue(queue) + rotateQueue(queue) +
+            "-".repeat(dashes) + "*".repeat(stars);
+
+        console.log(output);
     }
 }
+
+printDNA(4);
