074fa415aa2f932b9e039888e6dbc946c77ec55b
Uendy <alex.beshkov98@gmail.com>
2021-03-08 18:39:36 +0200

    Update: -Finished: Q10 DNA Helix.js // A bit more hardcoded, than I would have liked

--- JS Fundamentals/L03 Functions/L03 Qs/Q10 DNA Helix.js
+++ JS Fundamentals/L03 Functions/L03 Qs/Q10 DNA Helix.js
@@ -22,16 +22,48 @@ function printDNA(lines) {
         return temporary;
     }
 
+    function changeStars(stars, counter) {
+        switch (counter) {
+            case 0: return 2;
+            case 1: return 1;
+            case 2: return 0;
+            case 3: return 1;
+            case 4: return 2;
+        }
+    }
+
+    function changeDashes(dashes, counter) {
+        switch (counter) {
+            case 0: return 0;
+            case 1: return 2;
+            case 2: return 4;
+            case 3: return 2;
+            case 4: return 0;
+        }
+    }
+
     let stars = 2;
     let dashes = 0;
+    let counter = 0;
+
     for (let line = 0; line < lines; line++) {
 
-        let output = "*".repeat(stars) + "-".repeat(dashes) +
-            rotateQueue(queue) + rotateQueue(queue) +
-            "-".repeat(dashes) + "*".repeat(stars);
+        let output = "*".repeat(stars) +
+            rotateQueue(queue) +
+            "-".repeat(dashes) + 
+            rotateQueue(queue) +
+            "*".repeat(stars);
 
         console.log(output);
+
+        counter++;
+        if(counter === 4){
+            counter = 0;
+        }
+        stars = changeStars(stars, counter);
+        dashes = changeDashes(dashes, counter);
     }
 }
 
 printDNA(4);
+printDNA(10);
