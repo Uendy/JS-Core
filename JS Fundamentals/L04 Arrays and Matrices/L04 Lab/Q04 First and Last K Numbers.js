df6669e3e35127801b5f0609645e1ebf50ff2f05
Uendy <alex.beshkov98@gmail.com>
2021-03-09 21:33:55 +0200

    Adding: -Q04 First and Last K Numbers.js (started it but slice is being weird)

---
+++ JS Fundamentals/L04 Arrays and Matrices/L04 Lab/Q04 First and Last K Numbers.js
@@ -0,0 +1,14 @@
+function getFirstAndLastElements(array) {
+    let count = array.shift(0);
+    let first = array.splice(0, count);
+    let last = array.splice (array.length - count, count);
+
+    console.log(first.join(" "));
+    console.log(last.join(" "));
+}
+getFirstAndLastElements([2,
+    7, 8, 9]
+);
+getFirstAndLastElements([3,
+    6, 7, 8, 9]
+);
