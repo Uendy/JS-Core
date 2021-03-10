c48cdf9a629ac01ebd57e1a8aa1bb90982280453
Uendy <alex.beshkov98@gmail.com>
2021-03-10 11:52:49 +0200

    Update: -Finished: Q04 First and Last K Numbers.js
    Adding:
    -JSLINQ.docx (document to help me transition from LINQ)

--- JS Fundamentals/L04 Arrays and Matrices/L04 Lab/Q04 First and Last K Numbers.js
+++ JS Fundamentals/L04 Arrays and Matrices/L04 Lab/Q04 First and Last K Numbers.js
@@ -1,14 +1,10 @@
 function getFirstAndLastElements(array) {
     let count = array.shift(0);
-    let first = array.splice(0, count);
+    let first = array.slice(0, count);
     let last = array.splice (array.length - count, count);
 
     console.log(first.join(" "));
     console.log(last.join(" "));
 }
-getFirstAndLastElements([2,
-    7, 8, 9]
-);
-getFirstAndLastElements([3,
-    6, 7, 8, 9]
-);
+getFirstAndLastElements([2, 7, 8, 9]);
+getFirstAndLastElements([3, 6, 7, 8, 9]);
