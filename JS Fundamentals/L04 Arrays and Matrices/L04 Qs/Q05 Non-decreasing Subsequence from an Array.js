85b4ea80f84a6940a445e1f60f3305c8237b49ac
Uendy <alex.beshkov98@gmail.com>
2021-03-13 17:39:43 +0200

    Adding: -Q06 Double Sort Array.js

--- JS Fundamentals/L04 Arrays and Matrices/L04 Qs/Q05 Non-decreasing Subsequence from an Array.js
+++ JS Fundamentals/L04 Arrays and Matrices/L04 Qs/Q05 Non-decreasing Subsequence from an Array.js
@@ -8,13 +8,13 @@ function getIncreasingSeq(array) {
     }, Number.NEGATIVE_INFINITY);
     console.log(result.join("\n"));
 
-    let biggestNum = Number.NEGATIVE_INFINITY;
-    array.forEach(num => {
-        if(num > biggestNum){
-            biggestNum = num;
-            console.log(num);
-        }    
-    });
+    //let biggestNum = Number.NEGATIVE_INFINITY;
+    //array.forEach(num => {
+    //    if(num > biggestNum){
+    //        biggestNum = num;
+    //        console.log(num);
+    //    }    
+    //});
 }
 
 getIncreasingSeq([1, 3, 8, 4, 10, 12, 3, 2, 24]);
