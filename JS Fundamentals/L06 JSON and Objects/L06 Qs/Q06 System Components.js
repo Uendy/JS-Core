73a0da3edfb3b0919037820ae393e32f39bd31d3
Uendy <alex.beshkov98@gmail.com>
2021-04-02 20:00:17 +0300

    Update: -Finished Q06 System Components.js // This took me hours because when it cant to the 2nd compare(by alphabetical order as second criteria.) I didnt realize I had missed () while toLowerCasing one of the two

--- JS Fundamentals/L06 JSON and Objects/L06 Qs/Q06 System Components.js
+++ JS Fundamentals/L06 JSON and Objects/L06 Qs/Q06 System Components.js
@@ -1,4 +1,4 @@
-function swag(input) {
+function printNestedSystem(input) {
     let wholeSystem = new Map();
 
     input.forEach(line => {
@@ -15,12 +15,43 @@ function swag(input) {
         }
 
         wholeSystem.get(system).get(component).push(sub);
-    });
+    });    
 
-    // Sort them
+    function sortSystem(a, b, wholeSystem) {
+        // Sort them
+        // The Systems you’ve stored must be ordered by amount of components, in descending order, as first criteria,
+        // and by alphabetical order as second criteria. 
+        let aKey = wholeSystem.get(a).size;
+        let bKey = wholeSystem.get(b).size;
+        if(aKey > bKey){
+            return -1;
+        }
+        if(aKey < bKey){
+            return 1;
+        }
+        
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+    }
+
+    let orderedSystems = [...wholeSystem.keys()].sort((a, b) => sortSystem(a, b, wholeSystem));
+    for (let system of orderedSystems) {
+        console.log(system);
+
+        let prefix = "|||";
+        
+        // The Components must be ordered by amount of Subcomponents, in descending order.
+        let orderedComponent = [...wholeSystem.get(system).keys()].sort((a, b) => wholeSystem.get(system).get(b).length - wholeSystem.get(system).get(a).length);
+        for (let component of orderedComponent) {
+            console.log(prefix + component);
+
+            for (let subcomponent of wholeSystem.get(system).get(component)) {
+                console.log(prefix + prefix + subcomponent);
+            }
+        }
+    }
 }
 
-swag([
+printNestedSystem([
     'SULS | Main Site | Home Page',
     'SULS | Main Site | Login Page',
     'SULS | Main Site | Register Page',
