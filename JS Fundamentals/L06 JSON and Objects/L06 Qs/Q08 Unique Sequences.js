ad4fd94deed08b294f5c6201468b6b52639cb77f
Uendy <alex.beshkov98@gmail.com>
2021-04-05 19:13:43 +0300

    Update: -Finished: Q08 Unique Sequences.js

--- JS Fundamentals/L06 JSON and Objects/L06 Qs/Q08 Unique Sequences.js
+++ JS Fundamentals/L06 JSON and Objects/L06 Qs/Q08 Unique Sequences.js
@@ -1,5 +1,14 @@
 function getUniqueSequences(input) {
-    
+    let customSort = (arrA, arrB, map) => map.get(arrA) - map.get(arrB);
+    let arrays = new Map();
+    for (let line of input) {
+        let array = JSON.parse(line).map(Number).sort((a, b) => b - a);
+        let toStore = `[${array.join(', ')}]`;
+        if (!arrays.has(toStore))
+            arrays.set(toStore, array.length);
+    }
+
+    console.log([...arrays.keys()].sort((a, b) => customSort(a, b, arrays)).join('\n'));
 }
 
 getUniqueSequences([
