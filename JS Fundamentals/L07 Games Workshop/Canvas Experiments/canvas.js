5da4c230af6c5935d1ef324372e00cd9601d5ac3
Uendy <alex.beshkov98@gmail.com>
2021-04-08 18:33:39 +0300

    Update: -Added button and event to get title of image

--- JS Fundamentals/L07 Games Workshop/Canvas Experiments/canvas.js
+++ JS Fundamentals/L07 Games Workshop/Canvas Experiments/canvas.js
@@ -1,6 +1,14 @@
 let canvas = document.getElementById("can");
 let ctx = canvas.getContext("2d");
 
+
 let image = new Image();
 image.src = "./portrait.jpg";
 image.addEventListener("load", () => ctx.drawImage(image, 50, 50, 300, 450));
+
+
+let button = document.getElementById("myButton");
+button.addEventListener("click", function (){
+    alert("Portrait of a lady on fire");
+});
+
