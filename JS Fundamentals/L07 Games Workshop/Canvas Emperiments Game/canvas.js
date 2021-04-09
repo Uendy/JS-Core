155622edcd28296d474db37742e5e7a13f655203
Uendy <alex.beshkov98@gmail.com>
2021-04-09 20:15:52 +0300

    Update: - Did a couple more steps: Added the game loop and initialization of the sprites and score

--- JS Fundamentals/L07 Games Workshop/Canvas Emperiments Game/canvas.js
+++ JS Fundamentals/L07 Games Workshop/Canvas Emperiments Game/canvas.js
@@ -1,6 +1,15 @@
 let canvas = document.getElementById("can");
 let ctx = canvas.getContext("2d");
 
+// The main game loop
+var main = function () {
+	update();
+	render();
+
+	// Request to do this again ASAP
+	requestAnimationFrame(main);
+};
+
 // Get background 
 let bgReady = false;
 let background = new Image();
@@ -42,3 +51,73 @@ let monster = {
 	y: 0
 };
 let monstersCaught = 0;
+
+// Handle keyboard controls
+let keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+});
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+});
+
+// Reset the game when the player catches a monster
+let reset = function () {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
+
+// Update game objects
+let update = function () {
+	if (38 in keysDown) { // Player holding up
+		hero.y -= hero.speed;
+	}
+	if (40 in keysDown) { // Player holding down
+		hero.y += hero.speed;
+	}
+	if (37 in keysDown) { // Player holding left
+		hero.x -= hero.speed;
+	}
+	if (39 in keysDown) { // Player holding right
+		hero.x += hero.speed;
+	}
+
+	// Are they touching?
+	if (
+		hero.x <= (monster.x + 32)
+		& monster.x <= (hero.x + 32)
+		& hero.y <= (monster.y + 32)
+		& monster.y <= (hero.y + 32)
+	) {
+		++monstersCaught;
+		reset();
+	}
+};
+
+// Draw everything
+let render = function () {
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0);
+	}
+
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+
+	// Score
+	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Monsterrs caught: " + monstersCaught, 32, 32);
+};
