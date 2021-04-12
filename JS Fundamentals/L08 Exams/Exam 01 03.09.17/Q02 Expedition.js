7381b961f43488fc53e58ce6e82dd7e318a45ca9
Uendy <alex.beshkov98@gmail.com>
2021-04-12 21:27:30 +0300

    Update: -Did the primary map decypher, but haven't tested it yet

--- JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q02 Expedition.js
+++ JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q02 Expedition.js
@@ -1,6 +1,32 @@
 function findPath(primary, secondary, coordinates, start) {
     // Cycle each of the coordinates
-    // change the primary
+    for (let layOvers of coordinates) {
+        let x = layOvers[0];
+        let y = layOvers[1];
+
+        for (let row = x; row < primary.length && row < secondary[0].length; row++) {
+            let currentRow = primary[row];
+            for (let col = y; col < currentRow.length && col < secondary.length; col++) {
+                let secondRow = row - x;
+                let secondCol = col - y;
+
+                let firstMapCurrent = primary[row][col];
+                let secondMapCurrent = secondary[secondRow][secondCol];
+                
+                // change the primary
+                if(secondMapCurrent === 1){
+                    if(firstMapCurrent === 0){
+                        primary[row][col] = 1;
+                    }   
+                    else{
+                        primary[row][col] = 0;
+                    }
+                }
+                
+            }
+        }
+    }
+    
 
     // begin from start
     // caclulate steps
