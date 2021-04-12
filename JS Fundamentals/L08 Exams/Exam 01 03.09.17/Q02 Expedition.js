3958f8e451a761197ae1b09f0f4ed9a9cd5ddba2
Uendy <alex.beshkov98@gmail.com>
2021-04-12 20:57:13 +0300

    Adding: -Q02 Expedition.js (Began with schema)

---
+++ JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q02 Expedition.js
@@ -0,0 +1,29 @@
+function findPath(primary, secondary, coordinates, start) {
+    // Cycle each of the coordinates
+    // change the primary
+
+    // begin from start
+    // caclulate steps
+    // follow the 0's
+    // print (exit path, steps and direction) or (find quadrant, steps)
+}
+
+findPath([
+    [1, 1, 0, 1],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [1, 0, 1, 0]
+    ],
+   [
+    [0, 0, 1, 0, 1],
+    [1, 0, 0, 1, 1],
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1]
+   ],
+   [
+    [0, 0],
+    [2, 1],
+    [1, 0]
+   ],
+   [2, 0]
+   );
