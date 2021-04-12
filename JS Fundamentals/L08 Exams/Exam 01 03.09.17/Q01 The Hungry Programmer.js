56fe8fb405c39829cf5fac4b673b33ea38947583
Uendy <alex.beshkov98@gmail.com>
2021-04-12 17:59:49 +0300

    Update: -Did most of Q01 The Hungry Programmer.js, but only have it up to 75/100

--- JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q01 The Hungry Programmer.js
+++ JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q01 The Hungry Programmer.js
@@ -6,15 +6,67 @@ function calcFood(meals, commands) {
             break;
         }
 
-        // split command and check
-        // switch case and execute
+        if(meals.length === 0){
+            break;
+        }
+
+        let commandTokens = command.split(" ").filter(x => x !=="");
+        command = commandTokens[0];
+
+        switch(command){
+            case "Serve":
+                let servedMeal = meals.pop();
+                console.log(`${servedMeal} served!`);
+                break;
+
+            case "Add":
+                let foodAdded = commandTokens[1];
+                meals.unshift(foodAdded);
+                break;
+
+            case "Shift":
+                let firstIndex = commandTokens[1];
+                let secondIndex = commandTokens[2];
+
+                if(firstIndex >= 0 && firstIndex < commands.length &&
+                secondIndex >= 0 && secondIndex < commands.length){
+                    let temporary = meals[firstIndex];
+                    meals[firstIndex] = meals[secondIndex];
+                    meals[secondIndex] = temporary;
+                }
+                break;
+
+            case "Eat":
+                let foodEaten = meals.shift();
+                eaten++;
+                console.log(`${foodEaten} eaten`);
+                break;
+            
+            case "Consume":
+                let startIndex = commandTokens[1];
+                let endIndex = commandTokens[2];
+
+                if(startIndex >= 0 && endIndex < meals.length){
+                    let count = endIndex - startIndex + 1;
+                    meals.splice(startIndex, count);
+                    console.log("Burp!");
+                    eaten += count;
+                }
+                break;
+        }
     }
 
-    console.log(`Meals left: ${meals.joing(", ")}`);
+    if(meals.length === 0){
+        console.log("The food is gone");
+    }
+    else{
+        console.log(`Meals left: ${meals.join(", ")}`);
+    }
     console.log(`Meals eaten: ${eaten}`);
 }
 
-calcFood(['fries', 'fish', 'beer', 'chicken', 'beer', 'eggs'],
+calcFood(
+['fries', 'fish', 'beer', 'chicken', 'beer', 'eggs'],
 ['Add spaghetti',
  'Shift 0 1',
  'Consume 1 4',
