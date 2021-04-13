5aedbb8fb6cb416f3fb90c717a1fb38ca8421895
Uendy <alex.beshkov98@gmail.com>
2021-04-13 19:16:00 +0300

    Update: -I am done with the first draft and its not crashing, but gives me an exit when I should be at a dead end, but the steps are correct.

--- JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q02 Expedition.js
+++ JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q02 Expedition.js
@@ -28,59 +28,138 @@ function findPath(primary, secondary, coordinates, start) {
     }
     
     // begin from start
-    let log = [start];
     let steps = 1;
     let currentX = start[0];
     let currentY = start[1];
+    primary[currentX][currentY] = 1;
     let goOn = true;
     while(goOn){
-        
+        // if step = 1, dont check for exiting
+        if(steps === 1){
+            let direction = lookForPath(currentX, currentY, primary);
+
+            switch(direction){
+                case "down":
+                    currentX +=1;
+                break;
+                case "up":
+                    currentX -= 1;
+                break;
+                case "left":
+                    currentY -= 1;
+                break;
+                case "right":
+                    currentY += 1;
+                break;
+            }
+        }
+        else{
+            // if step > 2:
+            // Check for exit (border)
+            let exitFound = checkForExit(currentX, currentY, primary);
+            if(exitFound !== "none"){
+                console.log(steps);
+                console.log(exitFound);
+                return;
+            }
+
+            // check for 0 (path)
+            primary[currentX][currentY] = 1;
+            let direction = lookForPath(currentX, currentY, primary);
+            switch(direction){
+                // if found => turn current into 1 and move currentX and currentY, incrament steps
+                case "down":
+                    currentX +=1;
+                break;
+                case "up":
+                    currentX -= 1;
+                break;
+                case "left":
+                    currentY -= 1;
+                break;
+                case "right":
+                    currentY += 1;
+                break;
+                case "none":
+                // if not found => end with dead end
+                goOn = false;
+
+                console.log(steps);
+
+                // find quadrant:
+                let halfHeight = primary[0].length / 2;
+                let halfWidth = primary.length / 2;
+                // 1 (top right)
+                if(currentX < halfHeight && currentY >= halfWidth){
+                    console.log("Dead end 1");
+                }
+
+                // 2 (top left)
+                if(currentX < halfHeight && currentY < halfWidth){
+                    console.log("Dead end 2");
+                }
+
+                // 3 (bottom left)
+                if(currentX >= halfHeight && currentY < halfWidth){
+                    console.log("Dead end 3");
+                }
+
+                // 4 (bottom right)
+                if(currentX >= halfHeight && currentY >= halfWidth){
+                    console.log("Dead end 4");
+                }
+                break;
+            }
+        }
+        steps++;
     }
-    // caclulate steps
-    // follow the 0's
-    // print (exit path, steps and direction) or (find quadrant, steps)
 
     function lookForPath(x, y, primary) {
         // down: 
         let downX = x - 1;
-        if(primary[downX][y] === 0 && checkCoordinate(downX, "row", primary)){
+        if(primary[downX][y] === 0){
             return "down";
         }
 
         // up: 
         let upX = x + 1;
-        if(primary[upX][y] === 0 && checkCoordinate(upX, "row", primary)){
+        if(primary[upX][y] === 0){
             return "up";
         }
 
         // left: 
         let leftCol = y - 1;
-        if(primary[x][leftCol] === 0 && checkCoordinate(leftCol, "col", primary)){
+        if(primary[x][leftCol] === 0){
             return "left";
         }
 
         // right:
         let rightCol = y + 1;
-        if(primary[x][rightCol] === 0 && checkCoordinate(rightCol, "col", primary)){
+        if(primary[x][rightCol] === 0){
             return "right";
         }
+
+        return "none";
     }
 
-    function checkCoordinate(value, orientation, primary) {
-        if(orientation === "row"){
-            let rowMax = primary.length();
-            if(value >= 0 && value < rowMax){
-                return true;
-            }
-        }
-        else{
-            let colMax = primary[0].length();
-            if(value >= 0 && value < colMax){
-                return true;
-            }
+    function checkForExit(x, y, primary) {
+        if(x === 0){
+            return "Top";
         }
 
-        return false;
+        if(y === 0){
+            return "Left";
+        }
+
+        if(x === primary.length - 1){
+            return "Bottom";
+        }
+
+        if(y === primary[0].length - 1){
+            return "Right";
+        }
+
+        return "none";
     }
 }
 
