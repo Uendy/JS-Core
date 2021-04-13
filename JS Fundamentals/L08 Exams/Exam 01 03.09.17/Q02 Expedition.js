6e62427f74db8072ecbac822992bf5eb7e3fff0d
Uendy <alex.beshkov98@gmail.com>
2021-04-13 18:28:48 +0300

    Update: -I have complicated it too much

--- JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q02 Expedition.js
+++ JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q02 Expedition.js
@@ -27,11 +27,61 @@ function findPath(primary, secondary, coordinates, start) {
         }
     }
     
-
     // begin from start
+    let log = [start];
+    let steps = 1;
+    let currentX = start[0];
+    let currentY = start[1];
+    let goOn = true;
+    while(goOn){
+        
+    }
     // caclulate steps
     // follow the 0's
     // print (exit path, steps and direction) or (find quadrant, steps)
+
+    function lookForPath(x, y, primary) {
+        // down: 
+        let downX = x - 1;
+        if(primary[downX][y] === 0 && checkCoordinate(downX, "row", primary)){
+            return "down";
+        }
+
+        // up: 
+        let upX = x + 1;
+        if(primary[upX][y] === 0 && checkCoordinate(upX, "row", primary)){
+            return "up";
+        }
+
+        // left: 
+        let leftCol = y - 1;
+        if(primary[x][leftCol] === 0 && checkCoordinate(leftCol, "col", primary)){
+            return "left";
+        }
+
+        // right:
+        let rightCol = y + 1;
+        if(primary[x][rightCol] === 0 && checkCoordinate(rightCol, "col", primary)){
+            return "right";
+        }
+    }
+
+    function checkCoordinate(value, orientation, primary) {
+        if(orientation === "row"){
+            let rowMax = primary.length();
+            if(value >= 0 && value < rowMax){
+                return true;
+            }
+        }
+        else{
+            let colMax = primary[0].length();
+            if(value >= 0 && value < colMax){
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
 findPath([
