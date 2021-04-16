02e308e5ee158866b05c70c1bd42026921b2ab70
Uendy <alex.beshkov98@gmail.com>
2021-04-16 17:55:53 +0300

    Adding: -Q03 Lost in Mountains.js (start) -Q03 requirements to Exam 01 Qs.docx

---
+++ JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q03 Lost in the Mountains.js
@@ -0,0 +1,9 @@
+function decypherCode(keyword, text) {
+    
+}
+
+decypherCode("<>",
+    "o u%&lu43t&^ftgv><nortH4276hrv756dcc,  jytbu64574655k<>ThE sanDwich is iN the refrIGErator<>yl i75evEAsTer23,lfwe 987324tlblu6b");
+
+decypherCode("4ds",
+    "eaSt 19,432567noRt north east 53,123456north 43,3213454dsNot all those who wander are lost.4dsnorth 47,874532")
