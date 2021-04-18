6680b8fd8b78d92dea649d85127a70e46b6ce960
Uendy <alex.beshkov98@gmail.com>
2021-04-18 21:09:44 +0300

    Update: -Q04 Rest House.js is starting to take shape

--- JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q04 Rest House.js
+++ JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q04 Rest House.js
@@ -1,5 +1,45 @@
 function sortRooms(rooms, guests) {
     
+    // make the dataType:
+    //key = rooms(number), value = array holding the guests as objects
+    let hotel = new Map();
+
+    // get the rooms into the hotel 
+    for (let room of rooms) {
+        let currentRoomNumber = room.number;
+        let currentRoomSpread = room.type;
+
+        if(currentRoomSpread === "double-bedded"){
+            hotel.set(currentRoomNumber, [2]);
+        }
+        else{
+            hotel.set(currentRoomNumber, [3]);
+        }
+    }
+
+    let teaHouse = 0;
+
+    // go through the couples and put the guests in their places
+    for (let couple of guests) {
+        let firstPerson = couple.first;
+        let firstPersonName = firstPerson.name;
+        let firstPersonGender = firstPerson.gender;
+        let firstPersonAge = firstPerson.age;
+
+        let secondPerson = couple.second;
+        let secondPersonName = secondPerson.name;
+        let secondPersonGender = secondPerson.gender;
+        let secondPersonAge = secondPerson.age;
+        
+        // Find the best room for them:
+        //•	If the couple is a male and a female, they must be accommodated in the first free double room;
+        //•	If the couple is of two people of the same gender, they should be accommodated in a triple room;
+        //•	If there is free space in a triple room, it must be filled by a guest or guests of the same gender as the one already staying in the room;
+
+    }
+    
+
+    // sort 
 }
 
 sortRooms(
