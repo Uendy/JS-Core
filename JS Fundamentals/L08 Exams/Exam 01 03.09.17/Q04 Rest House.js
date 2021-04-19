b26269a67c4039e7562f5fd699f4304688e4b60c
Uendy <alex.beshkov98@gmail.com>
2021-04-19 19:48:53 +0300

    Update: -Went pretty deep in a solution, but I will have to redo it trying just using objects

--- JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q04 Rest House.js
+++ JS Fundamentals/L08 Exams/Exam 01 03.09.17/Q04 Rest House.js
@@ -10,10 +10,10 @@ function sortRooms(rooms, guests) {
         let currentRoomSpread = room.type;
 
         if(currentRoomSpread === "double-bedded"){
-            hotel.set(currentRoomNumber, Array(2));
+            hotel.set(currentRoomNumber, [0, 1]);
         }
         else{
-            hotel.set(currentRoomNumber, Array(3));
+            hotel.set(currentRoomNumber, [0, 1, 2]);
         }
     }
 
@@ -40,16 +40,93 @@ function sortRooms(rooms, guests) {
             // check for free double rooms:
             for (let [number, occupancy] of hotel) {
                 if(occupancy.length === 2){
-                    if(occupancy === undefined){
-                        console.log("yes");
+                    if(occupancy[0] === 0){
+                        occupancy[0] = firstPerson;
+                        occupancy[1] = secondPerson;
+                        continue;
                     }
                 }
             }
         }
-    }
-    
+        else{ // same gender
+            // try and find a free triple:
+            for (let [number, occupancy] of hotel){
+                if(occupancy.length === 3){
+                    if(occupancy[0] === 0){
+                        occupancy[0] = firstPerson;
+                        occupancy[1] = secondPerson;
+                        continue;
+                    }
+                }
+            }
+            // TO DO! : need to also place them in a free triple if any
+            // split them and find a triple with a free space and same gender
+            // find another triple with free space and same gender for the other
+            let firstPersonRoomFound = false;
+            let secondPersonRoomFound = false;
 
+            for (let [number, occupancy] of hotel) {
+                if(occupancy.length === 3){
+                    if(occupancy[2] === 0){
+                        if(occupancy[0].gender === firstPerson.gender){
+                            if(firstPersonRoomFound !== true){
+                                occupancy[2] = firstPerson;
+                                firstPersonRoomFound = true;
+                            }
+                            else if (secondPersonRoomFound !== true){
+                                occupancy[2] = secondPerson;
+                                secondPersonRoomFound = true;
+                            }
+                            
+                            if(firstPersonRoomFound && secondPersonRoomFound){
+                                continue;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // if no free house for one or both -> teahouse
+            if(firstPersonRoomFound === false){
+                teahouse++;
+            }
+            if(secondPersonRoomFound === false){
+                teahouse++;
+            }
+            
+        }
+    }
     // sort 
+    let sortedRooms = hotel.keys.sort;//((a.name, b.name) => localeCompare(a, b));
+    sortedRooms.forEach(room => {
+        //let currentRoom = hotel.get(room);
+        console.log(`Room number: ${room.number}`);
+
+        // sort guests by name (alphabetical)
+        for (let [key, guests] of room) {
+            // redo this and search for every guest if they are 0, 1, 2 and print if you have empty beds
+            if(guests === [0, 1]){
+                console.log(`Empty beds in the room: 2`);
+                continue;
+            }
+
+            if(guests === [0, 1, 2]){
+                console.log(`Empty beds in the room: 3`);
+                continue;
+            }
+
+            guests.forEach(guest => {
+                console.log(`Guest name: ${guest.name}`);
+                console.log(`Guest age: ${guest.age}`);
+            });
+
+            if(guests.length === 3 && guest){
+
+            }
+        }
+
+        console.log(`Guests moved to the tea house: ${teahouse}`);
+    });
 }
 
 sortRooms(
