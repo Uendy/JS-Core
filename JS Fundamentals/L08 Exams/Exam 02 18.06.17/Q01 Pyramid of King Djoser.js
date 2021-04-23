a06e97e5cf3801ce31d698d43b110eee0373c2a3
Uendy <alex.beshkov98@gmail.com>
2021-04-23 15:04:56 +0300

    Update: -Adding Q01 Pyramid of King Djoser.js (start, I cant quite grasp difference between step and increment)

---
+++ JS Fundamentals/L08 Exams/Exam 02 18.06.17/Q01 Pyramid of King Djoser.js
@@ -0,0 +1,14 @@
+function calculatePyramidMaterials(base,  incrament) {
+    let height = 0;
+    let stone = 0;
+    let marble = 0;
+    let lapiz = 0;
+    let gold = 0;
+
+    for (let step = base; step >= 1; index-=2) {
+    }
+}
+
+calculatePyramidMaterials(11, 1);
+calculatePyramidMaterials(11, 0.75);
+calculatePyramidMaterials(23, 0.5)
