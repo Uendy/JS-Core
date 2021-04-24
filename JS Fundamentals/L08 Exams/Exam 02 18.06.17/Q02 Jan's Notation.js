e647145ad817497334d9fe6fa1fe5a7314ccc551
Uendy <alex.beshkov98@gmail.com>
2021-04-24 16:28:43 +0300

    Update: -Getting close to finishing Q02 Jan's Notation.js // But got into a pickle navigating the keys of Map

--- JS Fundamentals/L08 Exams/Exam 02 18.06.17/Q02 Jan's Notation.js
+++ JS Fundamentals/L08 Exams/Exam 02 18.06.17/Q02 Jan's Notation.js
@@ -5,7 +5,7 @@ function calculateArrayElements(input) {
         let operand = input[index];
 
         let numbers = new Map();
-        for (let i = index; i > 0; i--){
+        for (let i = index - 1; i >= 0; i--){
             if(typeof input[i] === 'number'){
                 numbers.set(i, input[i]);
             }
@@ -15,33 +15,39 @@ function calculateArrayElements(input) {
             }
         }
 
-        if(numbers.keys.length !== 2){
+        if([...numbers.keys()].length !== 2){
             console.log("Error: too many operands!");
         }
         
         let keys = numbers.keys();
+        let firstNum = numbers.get(keys.next().value);
+        let secondNum = numbers.get(keys.next().value);
         let result = 0;
         switch (operand) {
             case "+":
-                result = numbers[keys][0] + numbers[keys][1];
+                result = firstNum + secondNum;
                 break;
             case "-":
-                result = numbers[keys][0] - numbers[keys][1];
+                result = firstNum - secondNum;
                 break;
             case "*":
-                result = numbers[keys][0] * numbers[keys][1];
+                result = firstNum * secondNum;
                 break;
             case "/":
-                result = numbers[keys][0] / numbers[keys][1];
+                result = firstNum / second;
                 break;
         }
 
+        // probably need to just re-make input, so that it is re-sized properly
         input[index] = result;
-        delete input[keys][0];
-        delete input[keys][1];
+        keys = numbers.keys();
+        let firstIndex = keys.next().value;
+        let secondIndex = keys.next().value;
+        delete input[firstIndex];
+        delete input[secondIndex];
     }
 
-    if(numbers.length > 1){
+    if(input.length > 1){
         console.log("Error: not enough operands!");
     }
     else{
