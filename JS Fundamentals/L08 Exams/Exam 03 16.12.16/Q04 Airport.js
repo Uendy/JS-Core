9dce386aa804d8eaa305c3cc0147c928e95cb4ea
Uendy <alex.beshkov98@gmail.com>
2021-04-29 19:17:23 +0300

    Update: -Finished Q04 Airport.js (and Exam 03 16.12.16)

--- JS Fundamentals/L08 Exams/Exam 03 16.12.16/Q04 Airport.js
+++ JS Fundamentals/L08 Exams/Exam 03 16.12.16/Q04 Airport.js
@@ -23,7 +23,38 @@ function generateTraffic(input) {
           planes.add(ID);
         }
       }
+
+      if(!towns.has(town)){
+        towns.set(town, {planes: [], "departures": 0, "arrivals": 0});
+      }
+
+      if (!towns.get(town).planes.includes(ID)) {
+        towns.get(town).planes.push(ID);
+      }
+
+      if (action == "land") {
+        towns.get(town).arrivals += passengers;
+      } else {
+        towns.get(town).departures += passengers;
+      }
     }
+
+    console.log("Planes left:");
+    [...planes].sort((p1, p2) => p1.localeCompare(p2)).forEach(p => console.log(`- ${p}`));
+    [...towns].sort((t1, t2) => {
+        if (t1[1].arrivals < t2[1].arrivals) return 1;
+        if (t1[1].arrivals > t2[1].arrivals) return -1;
+        return t1[0].localeCompare(t2[0]);
+    }).forEach(logData);
+
+    function logData(town) {
+      //console.log(`${town[0]} ${town[1].arrivals} ${town[1].departures}`);
+      console.log(town[0]);
+      console.log(`Arrivals: ${town[1].arrivals}`);
+      console.log(`Departures: ${town[1].departures}`);
+      console.log("Planes:");
+      town[1].planes.sort((p1, p2) => p1.localeCompare(p2)).forEach(p => console.log(`-- ${p}`));
+  }
 }
 
 generateTraffic([
